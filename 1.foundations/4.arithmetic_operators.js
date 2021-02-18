a6b204e3fdb610b21e5d78aa2b941cb315ecd1fe
Vitalii <vitaliy.rysich@gmail.com>
2021-02-18 23:45:05 +0200

    arithmetic operators

---
+++ 1.foundations/4.arithmetic_operators.js
@@ -0,0 +1,11 @@
+var addition = 2 + 2;
+var substraction = 3 - 90;
+var division = 10 / 5;
+var multiplication = 3 * 30;
+var remainder = 5 % 2;
+
+console.log(addition);
+console.log(substraction);
+console.log(division);
+console.log(multiplication);
+console.log(remainder);
