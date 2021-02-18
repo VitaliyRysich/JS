2840771478407749fefa65c66e14d77f05e0ee28
Vitalii <vitaliy.rysich@gmail.com>
2021-02-19 00:34:42 +0200

    switch

---
+++ 1.foundations/10.switch.js
@@ -0,0 +1,26 @@
+var person = {
+    name: "Anna",
+    age: 18
+  }
+  
+  switch(true) {
+    case (person.age == 17):
+      console.log(person.name + " is about to be an adult");
+      break;
+    case (person.age >= 18):
+        console.log(person.name + " is an adult");
+      break;
+    default:
+        console.log(person.name + "is not an adult");
+  }
+  
+  switch (new Date().getDay()) {
+    case 6:
+        console.log("Saturday")
+      break;
+    case 5:
+        console.log("Friday")
+      break;
+    default:
+        console.log("I don't know")
+  }
