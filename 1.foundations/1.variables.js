606d13cbf2a5f312d74e681c6a97b44d779169d8
Vitalii <vitaliy.rysich@gmail.com>
2021-02-18 22:58:26 +0200

    variables

---
+++ 1.foundations/1.variables.js
@@ -0,0 +1,14 @@
+var name = "Vitalii Rysich"; //string
+var age = 27; //number
+var height = 1.81; //number
+var hasDriverLicence = true; //boolean
+var empty = undefined; //undefined
+
+
+console.log(name + " type = " + typeof name);
+console.log(age + " type = " + typeof age);
+console.log(height + " type = " + typeof height);
+console.log(hasDriverLicence + " type = " + typeof hasDriverLicence);
+console.log(empty + " type = " + typeof empty);
+
+
