a58e8daa0ceb205d32110d6e9a8f7b5da6706445
Vitalii <vitaliy.rysich@gmail.com>
2021-02-19 14:58:27 +0200

    map, filter, reduce

---
+++ 1.foundations/13.map_filter_reduce.js
@@ -0,0 +1,21 @@
+var map = [1, 2, 3, 4, 5].map(function(n) {
+    return n * 2;
+});
+
+console.log("[].map")
+console.log(map);
+
+
+var filter = [1, 2, 3, 4, 5, 10, 46].filter(function(n) {
+    return n % 2 == 0;
+});
+console.log("[].filter");
+console.log(filter);
+
+
+var reduce = [1, 2, 3, 4, 5].reduce(function(accumulator, current){
+    return accumulator + current;
+});
+
+console.log("[].reduce");
+console.log(reduce);
