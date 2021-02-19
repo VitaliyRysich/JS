3dc216203828a81f8fc9621c8bf66cfbe07cc3ee
Vitalii <vitaliy.rysich@gmail.com>
2021-02-19 14:21:59 +0200

    comparison and logical operators

---
+++ 1.foundations/11.comparison_and_logical_operators.js
@@ -0,0 +1,18 @@
+console.log("Comparison Operators");
+console.log(10 == 10);
+console.log(10 < 10);
+console.log(10 <= 10);
+console.log(10 > 10);
+console.log(10 >= 10);
+console.log(10 != 10);
+
+console.log("Logical Operators");
+console.log(10 == 10 && 20 == 20); //and &&
+console.log(10 == 10 && 20 != 20);
+
+console.log(10 == 10 || 20 == 30); //or ||
+console.log(20 == 30 || 10 == 10);
+console.log(10 == 10 || 20 == 30);
+
+console.log(!(10 == 10)); //not !
+console.log(!(10 == 30));
