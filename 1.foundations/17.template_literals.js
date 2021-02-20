07d80ef9344d3774a5875b0c32e905030dd40ec0
Vitalii <vitaliy.rysich@gmail.com>
2021-02-20 23:43:52 +0200

    template literals

---
+++ 1.foundations/17.template_literals.js
@@ -0,0 +1,6 @@
+const name = 'Kate';
+const country = 'USA';
+const age = 30;
+
+console.log("Name: " + name + " Country: " + country + " Age: " + age);
+console.log(`Name: ${name} Country: ${country} Age: ${age}`);
