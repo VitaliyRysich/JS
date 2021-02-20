8f533ae0fc330cd7d06c930cb1c5569d5158fa71
Vitalii <vitaliy.rysich@gmail.com>
2021-02-20 23:20:49 +0200

    modules

---
+++ 2.modules/index.js
@@ -0,0 +1,11 @@
+import * as Math from './Math';
+import Animal from './Animal';
+
+console.log(Math.add(2,3));
+console.log(Math.subtract(2,3));
+console.log(Math.divide(2,3));
+console.log(Math.multiply(2,3));
+console.log(Math.PI);
+
+var animal = new Animal();
+console.log(animal.getClassType());
