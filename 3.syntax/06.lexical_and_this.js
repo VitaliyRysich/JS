9690640113e9f2b1f19a19bdc8b8e8bee0e76063
Vitalii <vitaliy.rysich@gmail.com>
2021-02-21 15:35:40 +0200

    lexical this

---
+++ 3.syntax/06.lexical_and_this.js
@@ -0,0 +1,11 @@
+const person = {
+    name: 'Alex',
+    cars: ['ferrari', 'lambo'],
+    toString: function(){
+        this.cars.forEach(car => {
+            console.log(`${this.name} has ${car}`);
+        });
+    }
+}
+
+person.toString();
