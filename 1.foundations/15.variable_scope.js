7ca914e9e42d29d79c980b9c5b27f3bd11efe896
Vitalii <vitaliy.rysich@gmail.com>
2021-02-21 15:06:04 +0200

    syntax

--- 1.foundations/15.variable_scope.js
+++
@@ -1,9 +0,0 @@
-for(var i = 0; i < 10; i++){
-    console.log("i: " + i);
-}
-console.log("i: " + i); //i: 10
-
-for(let j = 0; j < 10; j++){
-    console.log("j: " + j);
-}
-console.log("j: " + j); //j is not defined
