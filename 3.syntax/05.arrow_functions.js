3af90ec7dc3c40f75a08ba8683b7863f45acbf82
Vitalii <vitaliy.rysich@gmail.com>
2021-02-21 15:20:57 +0200

    arrow functions

---
+++ 3.syntax/05.arrow_functions.js
@@ -0,0 +1,37 @@
+// const hello = function() {
+//     const es6 = 'ES6';
+//     return `Hello ${es6}`;
+//   };
+
+const hello = () => {
+    const es6 = 'ES6';
+    return `Hello ${es6}`;
+  };
+
+  
+//   const powers = [1,2,3,4,5].map(function(number, index) {
+//     return Math.pow(number, index);
+//   });
+
+const powers = 
+    [1,2,3,4,5].map((number, index) => Math.pow(number, index));
+  
+  
+//   const add = function(n1, n2) {
+//     return n1 + n2;
+//   };
+
+const add = (n1, n2) => n1 + n2;
+  
+
+//   const milesToKm = function(miles) {
+//     return miles * 1.60934;
+//   };
+
+const milesToKm = miles => miles * 1.60934;
+  
+
+console.log(hello());
+console.log(powers);
+console.log(add(100,100));
+console.log(milesToKm(100));
